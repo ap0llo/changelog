5f8de8124d2b034879a53e2a08f72672d60d733f
Andreas Gruenwald <andreas@grynwald.net>
2020-04-25 18:52:43 +0200

    test: Add test to verify TemplateContainerBuildExtensions supports all defined template names

---
+++ src/ChangeLog.Test/EnumDataAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace Grynwald.ChangeLog.Test
+{
+    public class EnumDataAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            var parameters = testMethod.GetParameters();
+            if (parameters.Length == 1 && parameters[0].ParameterType.IsEnum)
+            {
+                foreach (var value in Enum.GetValues(parameters[0].ParameterType))
+                {
+                    yield return new object[] { value! };
+                }
+            }
+        }
+    }
+}
