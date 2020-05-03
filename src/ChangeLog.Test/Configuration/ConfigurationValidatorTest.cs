f440736bcdeb4e73793e9dbeb1c9d2c0e511ba86
Andreas Gruenwald <andreas@grynwald.net>
2020-05-03 18:41:52 +0200

    WIP: Validate configuration
    Add ConfigurationValidator

---
+++ src/ChangeLog.Test/Configuration/ConfigurationValidatorTest.cs
@@ -0,0 +1,30 @@
+﻿using Grynwald.ChangeLog.Configuration;
+using LibGit2Sharp;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace Grynwald.ChangeLog.Test.Configuration
+{
+    public class ConfigurationValidatorTest
+    {
+        private readonly ILogger<ConfigurationValidator> m_Logger = NullLogger<ConfigurationValidator>.Instance;
+
+
+        [Fact]
+        public void No_errors_are_found_in_default_configuration()
+        {
+            // ARRANGE
+            var sut = new ConfigurationValidator(m_Logger);
+
+            // ACT 
+            var valid = sut.Validate(ChangeLogConfigurationLoader.GetDefaultConfiguration());
+
+            // ASSERT
+            Assert.True(valid);
+        }
+
+
+
+    }
+}
---
+++ src/ChangeLog/Configuration/ConfigurationValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Grynwald.ChangeLog.Configuration;
+using Grynwald.ChangeLog.Model;
+using Microsoft.Extensions.Logging;
+
+namespace Grynwald.ChangeLog.Configuration
+{
+    internal class ConfigurationValidator
+    {
+        private readonly ILogger<ConfigurationValidator> m_Logger;
+
+        public ConfigurationValidator(ILogger<ConfigurationValidator> logger)
+        {
+            m_Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
+        public bool Validate(ChangeLogConfiguration configuration)
+        {
+            if (configuration is null)
+                throw new ArgumentNullException(nameof(configuration));
+
+
+            return true;
+        }
+
+    }
+}
