50a39860cd568360e1ce8558fbd44d385a3aa3f1
Andreas Gruenwald <andreas@grynwald.net>
2020-07-18 12:57:43 +0200

    test: Add tests for 'CommitMessageHeader' class, add helper class to generalize tests for implementations of IEquatable<T>

---
+++ src/ChangeLog.Test/ConventionalCommits/CommitMessageHeaderTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Grynwald.ChangeLog.ConventionalCommits;
+using Xunit;
+
+namespace Grynwald.ChangeLog.Test.ConventionalCommits
+{
+    /// <summary>
+    /// Tests for <see cref="CommitMessageHeader"/>
+    /// </summary>
+    public class CommitMessageHeaderTest : EqualityTest<CommitMessageHeader, CommitMessageHeaderTest>, IEqualityTestDataProvider<CommitMessageHeader>
+    {
+        public IEnumerable<(CommitMessageHeader left, CommitMessageHeader right)> GetUnequalTestCases()
+        {
+            yield return (
+                new CommitMessageHeader(CommitType.Feature, "Description"),
+                new CommitMessageHeader(CommitType.Feature, "Some other Description")
+            );
+
+            yield return (
+                new CommitMessageHeader(CommitType.Feature, "Description", "someScope"),
+                new CommitMessageHeader(CommitType.Feature, "Description", "someOtherScope")
+            );
+
+            yield return (
+                new CommitMessageHeader(CommitType.Feature, "Description"),
+                new CommitMessageHeader(CommitType.BugFix, "Description")
+            );
+
+            yield return (
+                new CommitMessageHeader(CommitType.Feature, "Description", scope: null, isBreakingChange: true),
+                new CommitMessageHeader(CommitType.Feature, "Description", scope: null, isBreakingChange: false)
+            );
+        }
+
+        public IEnumerable<(CommitMessageHeader left, CommitMessageHeader right)> GetEqualTestCases()
+        {
+            yield return (
+                new CommitMessageHeader(CommitType.Feature, "Description"),
+                new CommitMessageHeader(CommitType.Feature, "Description")
+            );
+
+            yield return (
+                new CommitMessageHeader(CommitType.Feature, "Description", scope: null, isBreakingChange: true),
+                new CommitMessageHeader(CommitType.Feature, "Description", scope: null, isBreakingChange: true)
+            );
+
+            yield return (
+                new CommitMessageHeader(CommitType.Feature, "Description", "someScope"),
+                new CommitMessageHeader(CommitType.Feature, "Description", "someScope")
+            );
+
+            yield return (
+                new CommitMessageHeader(CommitType.Feature, "Description", "someScope", isBreakingChange: true),
+                new CommitMessageHeader(CommitType.Feature, "Description", "someScope", isBreakingChange: true)
+            );
+        }
+
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        [InlineData("\t")]
+        public void Description_must_not_be_null_or_whitespace(string description)
+        {
+            Assert.Throws<ArgumentException>(() => new CommitMessageHeader(CommitType.Feature, description));
+            Assert.Throws<ArgumentException>(() => new CommitMessageHeader(CommitType.Feature, description, "someScope"));
+        }
+    }
+}
---
+++ src/ChangeLog.Test/EqualityTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Grynwald.ChangeLog.Test
+{
+    public interface IEqualityTestDataProvider<TTestee>
+    {
+        IEnumerable<(TTestee left, TTestee right)> GetEqualTestCases();
+
+        IEnumerable<(TTestee left, TTestee right)> GetUnequalTestCases();
+    }
+
+    /// <summary>
+    /// Base class for tests of <see cref="IEquatable{T}"/> implementations.
+    /// </summary>
+    public abstract class EqualityTest<TTestee, TDataProvider>
+        where TTestee : IEquatable<TTestee>
+        where TDataProvider : IEqualityTestDataProvider<TTestee>, new()
+    {
+        public static IEnumerable<object[]> EqualityTestCases()
+        {
+            var dataProvider = (IEqualityTestDataProvider<TTestee>)Activator.CreateInstance(typeof(TDataProvider))!;
+            foreach (var (left, right) in dataProvider.GetEqualTestCases())
+            {
+                yield return new object[] { left, right };
+            }
+        }
+
+        public static IEnumerable<object[]> InequalityTestCases()
+        {
+            var dataProvider = (IEqualityTestDataProvider<TTestee>)Activator.CreateInstance(typeof(TDataProvider))!;
+            foreach (var (left, right) in dataProvider.GetUnequalTestCases())
+            {
+                yield return new object[] { left, right };
+            }
+        }
+
+        public static IEnumerable<object[]> SampleInstance()
+        {
+            var dataProvider = (IEqualityTestDataProvider<TTestee>)Activator.CreateInstance(typeof(TDataProvider))!;
+            foreach (var (left, _) in dataProvider.GetUnequalTestCases())
+            {
+                yield return new object[] { left };
+                yield break;
+            }
+
+        }
+
+
+        [Theory]
+        [MemberData(nameof(EqualityTestCases))]
+        public void Comparision_of_two_equal_instances_yield_expected_result(TTestee left, TTestee right)
+        {
+            Assert.Equal(left.GetHashCode(), right.GetHashCode());
+            Assert.Equal(left, right);
+            Assert.True(left.Equals(right));
+            Assert.True(left.Equals((object)right));
+            Assert.True(right.Equals(left));
+            Assert.True(right.Equals((object)left));
+        }
+
+        [Theory]
+        [MemberData(nameof(InequalityTestCases))]
+        public void Comparision_of_two_unequal_instances_yield_expected_result(TTestee left, TTestee right)
+        {
+            Assert.NotEqual(left, right);
+            Assert.False(left.Equals(right));
+            Assert.False(left.Equals((object)right));
+            Assert.False(right.Equals(left));
+            Assert.False(right.Equals((object)left));
+        }
+
+        [Theory]
+        [MemberData(nameof(SampleInstance))]
+        public void Equals_retuns_false_if_argument_is_of_a_different_type(TTestee sut)
+        {
+            Assert.False(sut.Equals(new object()));
+        }
+    }
+}
