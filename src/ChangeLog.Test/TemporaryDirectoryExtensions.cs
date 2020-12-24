8d3cf7fa4db8a697bf51dc4999dd3d546118b2ed
Andreas Grünwald <andreas@grynwald.net>
2020-12-24 15:08:39 +0100

    Add RepositoryLocator that can be used to locate the root directory of a git repository

---
+++ src/ChangeLog.Test/TemporaryDirectoryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Grynwald.Utilities.IO;
+
+namespace Grynwald.ChangeLog.Test
+{
+    internal static class TemporaryDirectoryExtensions
+    {
+        public static string AddSubDirectory(this TemporaryDirectory temporaryDirectory, string relativePath)
+        {
+            if (Path.IsPathRooted(relativePath))
+                throw new ArgumentException("Path must not be rooted", nameof(relativePath));
+
+            var absolutePath = Path.Combine(temporaryDirectory, relativePath);
+            Directory.CreateDirectory(absolutePath);
+
+            return absolutePath;
+        }
+    }
+}
---
+++ src/ChangeLog/Git/RepositoryLocator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using LibGit2Sharp;
+
+namespace Grynwald.ChangeLog.Git
+{
+    /// <summary>
+    /// Helper class to find to locate git repositories
+    /// </summary>
+    public sealed class RepositoryLocator
+    {
+        /// <summary>
+        /// Attempts to get the root directory of a git repository stating from <paramref name="startingPath"/>.
+        /// </summary>
+        /// <remarks>
+        /// Searches <paramref name="startingPath"/> and any of its parent directories and checks if the directory is a valid git repository.
+        /// </remarks>
+        /// <param name="startingPath">The directory to start the search for a git repository.</param>
+        /// <param name="repositoryPath">On success, contains the root path of the git repository</param>
+        /// <returns>Returns <c>true</c> if a git repository was found, otherwise returns <c>false</c>.</returns>
+        public static bool TryGetRepositoryPath(string startingPath, [NotNullWhen(true)] out string? repositoryPath)
+        {
+            var path = Repository.Discover(startingPath);
+            if (String.IsNullOrEmpty(path))
+            {
+                repositoryPath = default;
+                return false;
+            }
+            else
+            {
+                repositoryPath = path;
+                return true;
+            }
+        }
+    }
+}
