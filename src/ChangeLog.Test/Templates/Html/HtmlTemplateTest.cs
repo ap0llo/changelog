7358a47225f645453c3972b63d8dad90582cb424
Andreas Gruenwald <andreas@grynwald.net>
2021-05-02 21:19:57 +0200

    Escape changelog text in Html output

--- src/ChangeLog.Test/Templates/Html/HtmlTemplateTest.cs
+++ src/ChangeLog.Test/Templates/Html/HtmlTemplateTest.cs
@@ -91,7 +91,6 @@ namespace Grynwald.ChangeLog.Test.Templates.Html
             Approve(changeLog, configuration);
         }
 
-
         [Fact]
         public void Table_of_contents_is_included_when_there_are_at_least_two_version()
         {
@@ -132,5 +131,69 @@ namespace Grynwald.ChangeLog.Test.Templates.Html
             Approve(changeLog);
         }
 
+        private class CodeTextElement : ITextElement
+        {
+            public TextStyle Style { get; set; }
+
+            public string Text { get; set; } = "";
+        }
+
+        [Fact]
+        public void Text_from_the_changelog_is_escaped_in_the_output_html()
+        {
+            // ARRANGE
+            var configuration = ChangeLogConfigurationLoader.GetDefaultConfiguration();
+            configuration.EntryTypes["feat"] = new() { DisplayName = "Features <1>" };
+            configuration.Footers["see-also"] = new() { DisplayName = "See & Also" };
+            configuration.Scopes["my-scope"] = new() { DisplayName = "My & Scope" };
+
+            var testData = new TestDataFactory();
+
+            var changeLog = new ApplicationChangeLog()
+            {
+                testData.GetSingleVersionChangeLog("1.2.3", entries: new[]
+                {
+                    testData.GetChangeLogEntry(
+                        type: "feat",
+                        scope: "my-scope",
+                        summary: "Some special characters & < >",
+                        isBreakingChange: true,
+                        body: new[]
+                        {
+                            "Description with special characters & < >"
+                        },
+                        footers :  new[]
+                        {
+                            new ChangeLogEntryFooter(new("see-also"), new PlainTextElement("some-text & < >")),
+                            new ChangeLogEntryFooter(new("see-also"), new CustomTextElement()
+                            {
+                                NormalizedText = "normalized text  & < >",
+                            }),
+                            new ChangeLogEntryFooter(new("see-also"), new CustomTextElement()
+                            {
+                                NormalizedStyle = TextStyle.Code,
+                                NormalizedText = "normalized text  & < >",
+                            }),
+                            new ChangeLogEntryFooter(new("see-also"), new CodeTextElement()
+                            {
+                                Style = TextStyle.Code,
+                                Text = "Some text  & < >",
+                            })
+                        }),
+                    testData.GetChangeLogEntry(type: "feat", summary: "Some other feature", breakingChangeDescriptions: new[]
+                    {
+                        "Breaking Change <1>",
+                        "Breaking Change <2>",
+                    }),
+                    testData.GetChangeLogEntry(type: "feat", summary: "Yet another feature", breakingChangeDescriptions: new[]
+                    {
+                        "Breaking Change <3>",
+                    })
+                })
+            };
+
+            // ACT / ASSERT
+            Approve(changeLog, configuration);
+        }
     }
 }
