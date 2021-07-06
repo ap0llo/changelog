198b6f62fb0b5dfb2726934e66ca94395915f5eb
Andreas Gruenwald <andreas@grynwald.net>
2021-07-06 21:21:13 +0200

    Add initial implementation of Graph data structure

---
+++ src/ChangeLog.Test/Pipeline/GraphTest.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Linq;
+using Grynwald.ChangeLog.Pipeline;
+using Xunit;
+
+namespace Grynwald.ChangeLog.Test.Pipeline
+{
+    /// <summary>
+    /// Tests for <see cref="Graph{T}"/>
+    /// </summary>
+    public class GraphTest
+    {
+        public class Nodes
+        {
+            [Fact]
+            public void Nodes_is_initially_empty()
+            {
+                // ARRANGE
+                var sut = new Graph<object>();
+
+                // ACT 
+
+                // ASSERT
+                Assert.NotNull(sut.Nodes);
+                Assert.Empty(sut.Nodes);
+            }
+        }
+
+        public class AddNode
+        {
+            [Fact]
+            public void Node_must_not_be_null()
+            {
+                // ARRANGE
+                var sut = new Graph<object>();
+
+                // ACT 
+                var ex = Record.Exception(() => sut.AddNode(null!));
+
+                // ASSERT
+                var argumentNullException = Assert.IsType<ArgumentNullException>(ex);
+                Assert.Equal("node", argumentNullException.ParamName);
+            }
+
+            [Fact]
+            public void AddNode_adds_node()
+            {
+                // ARRANGE
+                var node1 = new object();
+                var node2 = new object();
+
+                var sut = new Graph<object>();
+
+                // ACT 
+                sut.AddNode(node1);
+                sut.AddNode(node2);
+
+                // ASSERT
+                Assert.Equal(2, sut.Nodes.Count);
+                Assert.Contains(sut.Nodes, x => x == node1);
+                Assert.Contains(sut.Nodes, x => x == node2);
+            }
+
+            [Fact]
+            public void A_node_cannot_be_added_twice()
+            {
+                // ARRANGE
+                var node = new object();
+
+                var sut = new Graph<object>();
+
+                // ACT 
+                var added1 = sut.AddNode(node);
+                var added2 = sut.AddNode(node);
+
+                // ASSERT
+                Assert.True(added1);
+                Assert.False(added2);
+                var addedNode = Assert.Single(sut.Nodes);
+                Assert.Same(node, addedNode);
+            }
+        }
+
+        public class Edges
+        {
+            [Fact]
+            public void Edges_is_initially_empty()
+            {
+                // ARRANGE
+                var sut = new Graph<object>();
+
+                // ACT 
+
+                // ASSERT
+                Assert.NotNull(sut.Edges);
+                Assert.Empty(sut.Edges);
+            }
+        }
+
+        public class AddEdge
+        {
+            [Fact]
+            public void Nodes_must_not_be_null()
+            {
+                // ARRANGE
+                var sut = new Graph<object>();
+
+                // ACT / ASSERT
+                Assert.Throws<ArgumentNullException>(() => sut.AddEdge(new object(), null!));
+                Assert.Throws<ArgumentNullException>(() => sut.AddEdge(null!, new object()));
+            }
+
+            [Fact]
+            public void AddEdge_implicitly_adds_nodes()
+            {
+                // ARRANGE
+                var node1 = new object();
+                var node2 = new object();
+
+                var sut = new Graph<object>();
+
+                // ACT 
+                sut.AddEdge(node1, node2);
+
+                // ASSERT
+                Assert.Equal(2, sut.Nodes.Count);
+                Assert.Contains(sut.Nodes, x => x == node1);
+                Assert.Contains(sut.Nodes, x => x == node2);
+            }
+
+            [Fact]
+            public void Adds_expected_edges()
+            {
+                // ARRANGE
+                var node1 = new object();
+                var node2 = new object();
+                var node3 = new object();
+
+                var sut = new Graph<object>();
+
+                // ACT 
+                var added1 = sut.AddEdge(node1, node2);
+                var added2 = sut.AddEdge(node1, node3);
+
+                // ASSERT
+                Assert.True(added1);
+                Assert.True(added2);
+                Assert.Equal(2, sut.Edges.Count());
+                Assert.Collection(
+                    sut.Edges,
+                    e => Assert.Equal(new Graph<object>.Edge(node1, node2), e),
+                    e => Assert.Equal(new Graph<object>.Edge(node1, node3), e)
+                );
+            }
+
+            [Fact]
+            public void Edges_cannot_be_added_twice()
+            {
+                // ARRANGE
+                var node1 = new object();
+                var node2 = new object();
+
+                var sut = new Graph<object>();
+
+                // ACT 
+                var added1 = sut.AddEdge(node1, node2);
+                var added2 = sut.AddEdge(node1, node2);
+
+                // ASSERT
+                Assert.True(added1);
+                Assert.False(added2);
+                var edge = Assert.Single(sut.Edges);
+                Assert.Same(node1, edge.From);
+                Assert.Same(node2, edge.To);
+            }
+        }
+
+        public class GetAdjacentNodes
+        {
+            [Fact]
+            public void Throws_ArgumentNullException_is_node_is_null()
+            {
+                // ARRANGE
+                var sut = new Graph<object>();
+
+                // ACT 
+                var ex = Record.Exception(() => sut.GetAdjacentNodes(null!));
+
+                // ASSERT
+                var argumentNullException = Assert.IsType<ArgumentNullException>(ex);
+                Assert.Equal("node", argumentNullException.ParamName);
+            }
+
+            [Fact]
+            public void Throws_ArgumentNullException_if_not_is_not_found()
+            {
+                // ARRANGE
+                var node = new object();
+
+                var sut = new Graph<object>();
+
+                // ACT 
+                var ex = Record.Exception(() => sut.GetAdjacentNodes(node));
+
+                // ASSERT
+                var argumentNullException = Assert.IsType<ArgumentException>(ex);
+            }
+
+            [Fact]
+            public void Returns_empty_collection_if_node_has_no_neighbours()
+            {
+                // ARRANGE
+                var node = new object();
+
+                var sut = new Graph<object>();
+                sut.AddNode(node);
+
+                // ACT 
+                var adjacentNodes = sut.GetAdjacentNodes(node);
+
+                // ASSERT
+                Assert.Empty(adjacentNodes);
+            }
+
+            [Fact]
+            public void Returns_adjacent_nodes()
+            {
+                // ARRANGE
+                var node1 = new object();
+                var node2 = new object();
+                var node3 = new object();
+
+                var sut = new Graph<object>();
+                sut.AddEdge(node1, node2);
+                sut.AddEdge(node1, node3);
+
+                // ACT 
+                var adjacentNodes = sut.GetAdjacentNodes(node1);
+
+                // ASSERT
+                Assert.Equal(2, adjacentNodes.Count);
+                Assert.Contains(adjacentNodes, x => x == node2);
+                Assert.Contains(adjacentNodes, x => x == node3);
+            }
+        }
+
+    }
+}
---
+++ src/ChangeLog/Pipeline/Graph.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using Grynwald.Utilities.Collections;
+
+namespace Grynwald.ChangeLog.Pipeline
+{
+    /// <summary>
+    /// Represents a directed graph
+    /// </summary>
+    /// <typeparam name="T">The type of nodes in the graph.</typeparam>
+    internal class Graph<T> where T : notnull
+    {
+        public sealed class Edge : IEquatable<Edge>
+        {
+            public T From { get; }
+
+            public T To { get; }
+
+
+            public Edge(T from, T to)
+            {
+                From = from ?? throw new ArgumentNullException(nameof(from));
+                To = to ?? throw new ArgumentNullException(nameof(to));
+            }
+
+            public override int GetHashCode() => HashCode.Combine(To, From);
+
+            public override bool Equals(object? obj) => Equals(obj as Edge);
+
+            public bool Equals(Edge? other)
+            {
+                return other is not null &&
+                       From.Equals(other.From) &&
+                       To.Equals(other.To);
+            }
+        }
+
+
+        private readonly Dictionary<T, HashSet<T>> m_Edges = new Dictionary<T, HashSet<T>>();
+
+        /// <summary>
+        /// Gets all the graph's nodes
+        /// </summary>
+        public IReadOnlyCollection<T> Nodes { get; }
+
+        /// <summary>
+        /// Gets all the graph's edges
+        /// </summary>
+        public IEnumerable<Edge> Edges
+        {
+            get
+            {
+                foreach (var (fromNode, to) in m_Edges)
+                {
+                    foreach (var toNode in to)
+                    {
+                        yield return new(fromNode, toNode);
+                    }
+                }
+            }
+        }
+
+
+        public Graph()
+        {
+            Nodes = ReadOnlyCollectionAdapter.Create(m_Edges.Keys);
+        }
+
+
+        /// <summary>
+        /// Adds the specified node to the graph.
+        /// </summary>
+        /// <param name="node">The node to add</param>
+        /// <returns>Returns <c>true</c> is the node was added or <c>false</c> if the graph already contained the node.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <see cref="node"/> is <c>null</c>.</exception>
+        public bool AddNode(T node)
+        {
+            if (node is null)
+                throw new ArgumentNullException(nameof(node));
+
+            if (m_Edges.ContainsKey(node))
+                return false;
+
+            m_Edges.Add(node, new HashSet<T>());
+            return true;
+        }
+
+        /// <summary>
+        /// Adds the specified edge to the graph.
+        /// </summary>
+        /// <remarks>
+        /// Adding an edge will implicitly add <paramref name="to"/> and <paramref name="from"/> as nodes to the graph if the graph does not yet contain these nodes.
+        /// </remarks>
+        /// <param name="from">The edge's start node</param>
+        /// <param name="to">The edge's end node</param>
+        /// <returns>Returns <c>true</c> is the edge was added or <c>false</c> if the graph already contained the edge.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <see cref="from"/> of <see cref="to"/> is <c>null</c>.</exception>
+        public bool AddEdge(T from, T to)
+        {
+            if (from is null)
+                throw new ArgumentNullException(nameof(from));
+
+            if (to is null)
+                throw new ArgumentNullException(nameof(to));
+
+            AddNode(from);
+            AddNode(to);
+
+            return m_Edges[from].Add(to);
+        }
+
+        /// <summary>
+        /// Gets the adjacent nodes of the specified nodes.
+        /// </summary>
+        /// <remarks>
+        /// Gets all the nodes for which an edge from <see cref="node"/> to the node exists.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">Thrown when <see cref="node"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when the graph does not contain <see cref="node"/>.</exception>
+        public IReadOnlyCollection<T> GetAdjacentNodes(T node)
+        {
+            if (node is null)
+                throw new ArgumentNullException(nameof(node));
+
+            if (!m_Edges.ContainsKey(node))
+                throw new ArgumentException("Specified node was not found in the graph", nameof(node));
+
+            return m_Edges[node];
+
+        }
+    }
+}
