44124cadf80ea3c28c099e11a52ed3d2982e7a41
Andreas Grünwald <andreas@grynwald.net>
2022-10-14 23:59:02 +0200

    build: Do not download .NET 5 runtime during build
    .NET 5 runtime is no longer required to run tests (.NET 5 support was dropped with 4313af4)

--- build.ps1
+++ build.ps1
@@ -6,7 +6,6 @@ if ($env:TF_BUILD) {
 
 # Install .NET Core 3.1 and .NET 5 runtimes (requried for running tests on these platforms)
 ./build/dotnet-install.ps1 -Channel 3.1 -Runtime dotnet
-./build/dotnet-install.ps1 -Channel 5.0 -Runtime dotnet
 
 # Install SDK and runtime as specified in global.json
 ./build/dotnet-install.ps1 -JsonFile "$PSScriptRoot/global.json"
