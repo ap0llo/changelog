6617cb04b8e44b5c643f746efca9517d62091fae
Andreas Grünwald <andreas@grynwald.net>
2024-12-19 22:59:56 +0100

    Remove support for .NET 6 and .NET 7
    .NET 6 and .NET 7 have reached their end-of-life.
    
    BREAKING: Running changelog now required .NET 8 to be installed

--- build.ps1
+++ build.ps1
@@ -4,10 +4,6 @@ if ($env:TF_BUILD) {
     Write-Host "##[group]Install .NET SDK"
 }
 
-# Install .NET 6 and 7 runtimes (requried for running tests on this platform)
-./build/dotnet-install.ps1 -Channel 6.0 -Runtime dotnet
-./build/dotnet-install.ps1 -Channel 7.0 -Runtime dotnet
-
 # Install SDK and runtime as specified in global.json
 ./build/dotnet-install.ps1 -JsonFile "$PSScriptRoot/global.json"
 
